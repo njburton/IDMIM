8660ac2a092eaddc1aad619a23e0c81c5441720b
Katharina Wellstein <33021795+kwellstein@users.noreply.github.com>
2026-05-28 23:53:19 +0200

    bugfixes

--- fitModels.m
+++ fitModels.m
@@ -94,7 +94,7 @@ for iCondition = 1:nConditions
                     optionsFile.cohort(cohortNo).priorsFromRepetition);
             end
 
-            for iMouse  = 19:nSize
+            for iMouse  = 20:nSize
                 currMouse = optionsFile.cohort(cohortNo).mouseIDs{iMouse};
                 loadName = getFileName(optionsFile.cohort(cohortNo).testTaskPrefix,currTask,...
                     [],currCondition,iRep,nReps,[]);
