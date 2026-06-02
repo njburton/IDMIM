2b24decea767b7d896ecf36c3c30354f2a0c1237
Katharina Wellstein <33021795+kwellstein@users.noreply.github.com>
2026-06-02 08:40:28 +0200

    ...

--- simData_fitModels.m
+++ simData_fitModels.m
@@ -81,7 +81,7 @@ sim = load([optionsFile.paths.cohort(cohortNo).simulations,optionsFile.dataFiles
 % looping across tasks, samples, models that created the simulated behaviour (gen model | m_in)
 % and models that will be fitted to the simulated behaviour (estimating model | m_est)
 for iTask = 2:nTasks
-    for iSample = 1:nSamples
+    for iSample = 2:nSamples
         for m_in = 1:nModels
             for m_est =1:nModels
                 disp(['Model inversion for agent: ', num2str(iSample), ' | gen model ', 			optionsFile.modelSpace(m_in).name, ' | estimating with model: ', 				optionsFile.modelSpace(m_est).name]);
